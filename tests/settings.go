698f40a12a85fd03c32faf6c3bd34223a5e14b96
Dmitry Fedotov <FedotovQA@yandex.ru>
2024-11-28 14:34:41 +0300

    token

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,6 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
+
+// Тут нужно вставить свой актуальный токен
 var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzI4MjE5OTUsInBhc3N3b3JkSGFzaCI6IjM2MzkzNjMxMzI2NjYyMzczNTM1NjQzNjM2NjIzNDYxMzIzNzM1MzgzOTM2MzkzODMxMzgzNzM0NjMzNDM2MzIzMTMwNjYzNDYxNjY2MjYxNjMzNzM2MzczMzYyNjM2MjMwNjM2NTM0MzA2NDMzNjMzNjYxMzAzMTM2MzA2NDM1In0.Pc9tebN_l5MclX6mHzeIDM5p8hYF6GAny0FHfRBxg_A`
